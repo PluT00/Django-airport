aaec422f594e7804b379ae4ac661132fe59f2101
PluT00 <valeran2003@mail.ru>
2019-05-17 17:36:37 +0300

    Models Flight, Plane added

--- airport/crm/admin.py
+++ airport/crm/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from crm.models import Flight, Plane
 
-# Register your models here.
+
+class FlightAdmin(admin.ModelAdmin):
+    list_display = [
+        'flight_id',
+        'departure_time',
+        'company',
+        'plane_name',
+        'is_departure',
+        'status'
+    ]
+    search_fields = [
+        'flight_id',
+        'company',
+        'status'
+    ]
+
+admin.site.register(Flight, FlightAdmin)
+
+
+class PlaneAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+admin.site.register(Plane, PlaneAdmin)
--- airport/crm/models.py
+++ airport/crm/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Flight(models.Model):
+    STATUS_CHOISES = (
+        ('RG', 'Registration'),
+        ('OW', 'On the way'),
+        ('DL', 'Delayed'),
+        ('CN', 'Canceled'),
+        ('AR', 'Arrived'),
+        ('DP', 'Departure')
+    )
+    departure_time = models.DateTimeField(auto_now=False, auto_now_add=False)
+    flight_id = models.CharField(max_length=10)
+    country = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    arrival_time = models.DateTimeField(auto_now=False, auto_now_add=False)
+    company = models.CharField(max_length=50)
+    plane_name = models.ForeignKey(
+        'Plane',
+        on_delete=models.CASCADE,
+        related_name="flights"
+    )
+    status = models.CharField(
+        max_length=2,
+        choices=STATUS_CHOISES,
+        default='RG'
+    )
+    is_departure = models.BooleanField(default=True)
+
+    def __str__(self):
+        return '{0}'.format(self.flight_id)
+
+
+class Plane(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return '{0}'.format(self.name)
