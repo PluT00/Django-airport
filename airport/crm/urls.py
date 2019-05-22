c83d0eef40c7dda7f7858c49bfaba3033ae92061
PluT00 <valeran2003@mail.ru>
2019-05-23 00:28:40 +0300

    Functionality added

--- airport/crm/urls.py
+++ airport/crm/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
+from crm import views
+
 
 urlpatterns = [
-
+    path('', views.flights_list, name="flights_list_url"),
+    path('flight/<str:slug>/', views.flight_details, name="flight_details_url"),
 ]
--- airport/crm/views.py
+++ airport/crm/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from crm.models import Flight
+
+
+def flights_list(request):
+    table_id = 0
+    arrivals = []
+    departures = []
+    flights = Flight.objects.all()
+    for flight in flights:
+        if flight.is_departure:
+            departures.append(flight)
+        else:
+            arrivals.append(flight)
+    return render(request, 'crm/flights_list.html', context={
+        'departures': departures,
+        'arrivals': arrivals,
+        'ti': table_id
+    })
+
+
+def flight_details(request, slug):
+    flight = get_object_or_404(Flight, slug__iexact=slug)
+    return render(request, 'crm/flight_details.html', context={'flight': flight})
