3ecffee59e8a561dab2fe83862a99ad907210e3a
PluT00 <valeran2003@mail.ru>
2019-05-30 23:23:24 +0300

    Add ticket booking

--- airport/crm/urls.py
+++ airport/crm/urls.py
@@ -6,4 +6,9 @@ from crm import views
 urlpatterns = [
     path('', views.flights_list, name="flights_list_url"),
     path('flight/<str:slug>/', views.flight_details, name="flight_details_url"),
+    path(
+        'flight/<str:slug>/ticket/create/',
+        views.FlightTicket.as_view(),
+        name="flight_ticket_url"
+    )
 ]
--- airport/crm/views.py
+++ airport/crm/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404
+from django.views import View
 
-from crm.models import Flight
+from crm.models import Flight, Ticket
 
 
 def flights_list(request):
@@ -24,9 +26,38 @@ def flights_list(request):
 
 def flight_details(request, slug):
     flight = get_object_or_404(Flight, slug__iexact=slug)
+    tickets = Ticket.objects.filter(flight=flight).count()
+    free_seats = flight.plane_name.seats - tickets
     return render(request,
         'crm/flight_details.html',
         context={
-            'flight': flight
+            'flight': flight,
+            'free_seats': free_seats,
+            'seats': flight.plane_name.seats
         }
     )
+
+
+class FlightTicket(View):
+
+    def get(self, request, slug):
+        flight = Flight.objects.get(slug__iexact=slug)
+        return render(
+            request,
+            'crm/ticket_booking.html',
+            context={
+                'flight': flight
+            }
+        )
+
+    def post(self, request, slug):
+        current_flight = Flight.objects.get(slug__iexact=slug)
+        current_user = self.request.user
+        tickets = Ticket.objects.filter(flight=current_flight).count()
+        seats = current_flight.plane_name.seats
+        if tickets < seats:
+            new_ticket = Ticket.objects.create(
+                user=current_user,
+                flight=current_flight
+            )
+        return redirect('flights_list_url')
