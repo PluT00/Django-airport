adb9c0e7620188b88fdd600f4502d2f9815080a4
PluT00 <valeran2003@mail.ru>
2019-08-27 03:56:11 +0300

    switch to postgresql

--- airport/settings.py
+++ airport/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'airport.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djairport',
+        'USER': 'djairportuser',
+        'PASSWORD': 'xHNxb3gCz',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
