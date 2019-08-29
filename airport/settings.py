121cb8ec9cec69bf42fcafb5180f799998d7cac2
PluT00 <valeran2003@mail.ru>
2019-08-29 18:42:54 +0300

    Delete SECRET_KEY

--- airport/settings.py
+++ airport/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ef36r**vp20y8rurnu8kykx2#41^*ndfd0x5h)j2-x2_ust(q3'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
