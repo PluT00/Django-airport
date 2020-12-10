be43b26830c5301e9f65c2a918c5d03c3dd9aa95
PluT00 <valeran2003@mail.ru>
2020-12-11 00:28:21 +0300

    Update db setup

--- airport/settings.py
+++ airport/settings.py
@@ -83,11 +83,11 @@ WSGI_APPLICATION = 'airport.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangoairport',
-        'USER': 'djairportuser',
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'NAME': os.environ.get('POSTGRES_DB', ''),
+        'USER': os.environ.get('POSTGRES_USER', ''),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
+        'HOST': os.environ.get('POSTGRES_HOST', ''),
+        'PORT': os.environ.get('POSTGRES_POST', ''),
     }
 }
 
